0a7aee78e052dd0fb8b62e9fbadb3505eed7512b
mak962753 <mak962753@gmail.com>
2020-04-04 13:29:10 +0300

    updated parallax speed, updated models, added logo model

--- src/components/views/scene/parallax.ts
+++ src/components/views/scene/parallax.ts
@@ -8,11 +8,11 @@ export function initParallax(id: string): {(): void} {
         
         const _mouseY = e.clientY;
         const _depth1 = `50% 50%`;
-        const _depth2 = `${(_mouseX - _w) * 0.02}px ${50 - (_mouseY - _h) * 0.02}%`;
-        const _depth3 = `${(_mouseX - _w) * 0.04}px ${50 - (_mouseY - _h) * 0.04}%`;
-        const _depth4 = `${(_mouseX - _w) * 0.06}px ${50 - (_mouseY - _h) * 0.06}%`;
+        const _depth2 = `${(_mouseX - _w) * 0.004}px ${50 - (_mouseY - _h) * 0.004}%`;
+        const _depth3 = `${(_mouseX - _w) * 0.008}px ${50 - (_mouseY - _h) * 0.008}%`;
+        const _depth4 = `${(_mouseX - _w) * 0.012}px ${50 - (_mouseY - _h) * 0.012}%`;
         const x = [_depth4, _depth3, _depth2, _depth1].join(', ');
-        console.log(x);
+        //console.log(x);
         if (elem) 
             elem.style.backgroundPosition = x;
     }
