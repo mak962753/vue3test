de350be56973c1bff4c8e2e15fbab9f3bdea5899
mak962753 <mak962753@gmail.com>
2020-04-24 01:03:42 +0300

    ++

--- src/components/views/canv.vue
+++ src/components/views/canv.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="canv">
-        <canvas id="canvas" width="200" height="200">
+        <canvas id="canvas" width="400" height="300">
             This text is displayed if your browser does not support HTML5 Canvas.
         </canvas>
     </div>
@@ -18,14 +18,41 @@
                 const ctx = my_canvas!.getContext("2d");
                 ctx!.fillStyle = "orange";
                 ctx!.strokeStyle = "#000";
-                const {width: w, height: h} = my_canvas;
-                const c = new THREE.Vector2(w/2, h/2);
-                const r = 50;
-                const p = new THREE.Vector2(r, 0).add(c);
-                let v = o(new THREE.Vector2().subVectors(p, c)).multiplyScalar(5.5);
-                
 
-                function o(v: THREE.Vector2) : THREE.Vector2 {
+                const {width: w, height: h} = my_canvas;
+
+
+
+
+                const start: [number, number] = [w * 0.05, h];
+                const end: [number, number] = [w, h * 0.05];
+
+                const r = 1.1 * (w + h) / 2
+
+                const c = new THREE.Vector2().fromArray(circleCenter(start, end, r));
+
+                const p = new THREE.Vector2().fromArray(start);
+
+                let v = ortho(new THREE.Vector2().subVectors(p, c)).multiplyScalar(5.5);
+
+                function circleCenter([x1, y1]: [number, number], [x2,y2]: [number, number], r:number): [number, number] {
+                    const rr = r*r;
+                    const q = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
+                    const qq = q * q / 4;
+                    const y3 = (y1+y2)/2;
+                    const x3 = (x1+x2)/2;
+                    const baseX = Math.sqrt(rr - qq) * (y1-y2) / q; //calculate once
+                    const baseY = Math.sqrt(rr - qq) * (x2-x1) / q; //calculate once
+
+                    const centerX1 = x3 + baseX; //center x of circle 1
+                    const centerY1 = y3 + baseY; //center y of circle 1
+                    return [centerX1, centerY1];
+                    //const centerx2 = x3 - baseX; //center x of circle 2
+                    //const centery2 = y3 - baseY; //center y of circle 2
+                }
+
+
+                function ortho(v: THREE.Vector2) : THREE.Vector2 {
                     return new THREE.Vector2(-v.y, v.x).normalize();
                 }
                 function applyCircularAcc(center: THREE.Vector2, pos: THREE.Vector2, v: THREE.Vector2, r: number): THREE.Vector2 {
@@ -61,6 +88,8 @@
 
 <style lang="scss">
     [id=canv] {
+        display: inline-block;
+        border: 1px solid #666;
         margin-top: 60px;
     }
 </style>
